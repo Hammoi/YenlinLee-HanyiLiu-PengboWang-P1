9a2191e07330d0dc3842e1d05c62999b642ffc60
Samuel74028 <samuel.yenlin@gmail.com>
2021-11-30 14:09:40 -0800

    change the menubar to php file

---
+++ page1.php
@@ -0,0 +1,55 @@
+<?php
+  include_once 'menubar.php'
+  ?>
+<link rel="stylesheet" href="page1.css">
+    <!--content-->
+    <div class="content">
+      <img
+        src="https://static01.nyt.com/images/2021/08/03/sports/03olympics-basketball1/03olympics-basketball1-jumbo.jpg?quality=75&auto=webp"
+        class="img-fluid"
+        alt="Responsive image"
+      />
+      <h1>Name of Sport</h1>
+      <p>Brief description of sport......</p>
+      <br />
+      <h1>Goal of Sport</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil dolor qui
+        beatae, aspernatur facere atque ratione provident, esse quas cum quidem,
+        vero soluta! Ipsa modi omnis optio dolorum soluta aliquam dolorem!
+        Repellendus voluptas maiores similique totam tenetur! Eveniet eligendi
+        fugit illo blanditiis consequuntur eaque, reprehenderit praesentium.
+        Repellendus voluptatibus ad itaque?
+      </p>
+      <br />
+      <h1>Rules</h1>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ad alias est
+        repellat. Officiis debitis quaerat velit quod quam nam quasi suscipit
+        explicabo quas vel sit atque temporibus nisi officia ratione corporis
+        eum voluptatem distinctio, deleniti consequuntur earum. Unde culpa
+        impedit harum iure ratione. Deleniti quos eligendi soluta iusto sit
+        cupiditate.
+      </p>
+      <h1>Equipment</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis,
+        recusandae fugiat velit asperiores optio exercitationem aspernatur
+        quidem soluta fuga! Aspernatur dolore excepturi quod aut sequi
+        voluptatibus eaque facere fuga nisi, culpa dolorum vitae dignissimos
+        consequatur sint. Obcaecati, repellendus, voluptate nesciunt quam
+        laboriosam necessitatibus nam ratione, debitis dicta temporibus sed
+        expedita.
+      </p>
+      <br />
+      <h1>How to Win</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores,
+        deserunt numquam praesentium cumque eius dolorem nobis! Atque nisi natus
+        dolores, nam eum corrupti perspiciatis sequi maxime dicta fugiat quod
+        porro a odit distinctio pariatur, enim esse nulla dignissimos similique
+        vitae illum nostrum repellendus, officia quaerat. Veniam assumenda
+        adipisci culpa sint!
+      </p>
+    </div>
+  </body>
