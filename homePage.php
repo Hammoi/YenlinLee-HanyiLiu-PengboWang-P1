1e382a8bd8717363cffd2baa2ea6ad6d638ec531
Hanyi Liu <35943468+Hammoi@users.noreply.github.com>
2021-12-02 10:08:18 -0800

    Added style

---
+++ homePage.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'menubar.php'
+?>
+
+  <!--Actual site begins here-->
+  <div class="container">
+    <img src="https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/sports-1584678012.jpg" alt="Snow" style="width:100%;">
+    <div class="centered">Centered</div>
+</div>
