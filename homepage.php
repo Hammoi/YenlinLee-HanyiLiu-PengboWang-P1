266394eeb3e3636b7bf0ef8d78785ad409a6a5e2
Samuel74028 <samuel.yenlin@gmail.com>
2021-12-02 10:00:56 -0800

    link the menubar to homepage using php

---
+++ homepage.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'menubar.php'
+?>
+
+<!--Actual site begins here-->
+  <img src="https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/sports-1584678012.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*" class="img-fluid" alt="Responsive image">
