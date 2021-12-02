3f7d9ddf9e4130339e05c13e736b7aea9c485685
Hanyi Liu <35943468+Hammoi@users.noreply.github.com>
2021-12-02 10:08:41 -0800

    Update homePage.php

--- homePage.php
+++ homePage.php
@@ -2,8 +2,5 @@
 require_once 'menubar.php'
 ?>
 
-  <!--Actual site begins here-->
-  <div class="container">
-    <img src="https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/sports-1584678012.jpg" alt="Snow" style="width:100%;">
-    <div class="centered">Centered</div>
-</div>
+<!--Actual site begins here-->
+  <img src="https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/sports-1584678012.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*" class="img-fluid" alt="Responsive image">
