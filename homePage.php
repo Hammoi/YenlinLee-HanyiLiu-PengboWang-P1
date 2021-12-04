2f0c18f505e7610c327fac51c4817c468315a4ea
Hanyi Liu <hanyiliu0@gmail.com>
2021-12-03 21:47:32 -0800

    Added center text

--- homePage.php
+++ homePage.php
@@ -3,4 +3,9 @@ require_once 'menubar.php'
 ?>
 
 <!--Actual site begins here-->
-  <img src="https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/sports-1584678012.jpg?crop=1.00xw:1.00xh;0,0&resize=1200:*" class="img-fluid" alt="Responsive image">
+
+<link rel="stylesheet" href="style.css">
+<div class="container">
+    <img src="https://kubrick.htvapps.com/htv-prod-media.s3.amazonaws.com/images/sports-1584678012.jpg" alt="Snow" style="width:100%;">
+    <div class="centered">Centered</div>
+</div>
