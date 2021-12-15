672b1f8f2808b80e41d674b13e143b3b11f9080f
Samuel74028 <samuel.yenlin@gmail.com>
2021-12-15 15:14:08 -0800

    put in content and layout

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const hero = document.querySelector(".hero");
 //image and word
-const slider = document.querySelector(".slider");
+const backgroundColor = document.querySelector(".backgroundColor");
 //fancy background color
 const logo = document.querySelector("#Logo");
 //sport
@@ -18,16 +18,41 @@ tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
     { width: "80%", ease: Power2.easeInOut }
   )
   .fromTo(
-    slider,
+    backgroundColor,
     1.2,
     { x: "-100%" },
     { x: "0%", ease: Power2.easeInOut },
     "-=1.2"
-  )
-  .fromTo(home, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-+0.45")
-  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-+0.5")
-  .fromTo(column1, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-+0.5")
-  .fromTo(column2, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-+0.55")
-  .fromTo(column3, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-+0.6")
-  .fromTo(column4, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-+0.65")
-  .fromTo(column5, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-+0.7");
+  );
+//.fromTo(home, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-+0.5")
+
+//content animation
+const sliders = document.querySelectorAll(".sliders");
+const appearOptions = {
+  threshold: 0.3,
+  rootMargin: "0px 0px 0px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (
+  entries,
+  appearOnScroll
+) {
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting) {
+      return;
+    } else {
+      entry.target.classList.add("appear");
+      appearOnScroll.unobserve(entry.target);
+      console.log("appear");
+    }
+  });
+},
+appearOptions);
+
+sliders.forEach((slider) => {
+  appearOnScroll.observe(slider);
+});
+
+function myFunction(x) {
+  x.classList.toggle("change");
+}
